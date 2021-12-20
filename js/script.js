4d48fbd634ac4ceb743ae9912f013971c841e129
COMPLEJOZ <gabrielperseverance@gmail.com>
2021-12-20 13:16:14 -0500

    Primer repositorio

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let im1 = document.getElementById("im1");
+let im2 = document.getElementById("im2");
+let im3 = document.getElementById("im3");
+let im4 = document.getElementById("im4");
+
+im4.addEventListener("click", ()=> {
+    alert("Bien hecho");
+});
